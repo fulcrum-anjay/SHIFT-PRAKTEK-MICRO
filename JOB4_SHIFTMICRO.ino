8fe1a5c87d832b27f3d67ccfbb5146831003450f
fulcrum-anjay <saputridwi012@gmail.com>
2026-09-16 22:50:12 +0700

    Add files via upload

---
+++ JOB4_SHIFTMICRO.ino
@@ -0,0 +1,24 @@
+
+int led = 13;
+int pushButton = 2;
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(pushButton, INPUT);
+    pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  
+  int buttonState = digitalRead(pushButton);
+  Serial.println(buttonState);
+  if (buttonState == 1) {
+      digitalWrite(led, HIGH);   
+  } else {
+    digitalWrite(led, LOW);   
+  }
+  delay(1);        
+  
+}//laper + ngantuk = rasa ingin kms
